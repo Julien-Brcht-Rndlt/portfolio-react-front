056eacd1baa8daf8d05c64b696683c1f3e95eeda
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-07-21 18:05:07 +0200

    adding state for front backoffice editing

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,9 +1,42 @@
+import { useState } from "react";
+
 export default function AboutMe({ firstname, lastname, role, about}) {
+    const [myRole, setMyRole] = useState({
+        value: role,
+        editable: false,
+    });
+    const [myAbout, setMyAbout] = useState({
+        value: about,
+        editable: false,
+    });
     return (
+      <div>
         <div>
-            <div>{firstname}</div>
-            <div>{role}</div>
-            <div>{about}</div>
+          <h3>{firstname}</h3></div>
+        <div>
+          {
+            myRole.editable ?
+            (
+              <input value={myRole.value} />
+            )
+            : 
+            (
+              <h4>{role}</h4>
+            )
+          }
         </div>
+        <div>
+        {
+          myAbout.editable ?
+          (
+            <textarea value={myAbout.value} />
+          )
+          : 
+          (
+            <p>{about}</p>
+          )
+        }
+        </div>
+      </div>
     );
 }

e5df5cd43df6702b3a7cbe130a4140162c4fd8bd
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-07-22 18:33:50 +0200

    applying login + context + isAdmin display or not (more)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import './App.css';
+import { useState } from 'react';
 import AppRouter from './AppRouter';
+import AdminContext from './contexts/AdminContext';
 
 function App() {
+  const [isAdmin, setIsAdmin] = useState(false);
   return (
-    <AppRouter />
+    <AdminContext.Provider value={{ isAdmin, setIsAdmin }}>
+      <AppRouter />
+    </AdminContext.Provider>
   );
 }
 
--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { Icon } from 'semantic-ui-react';
 import axios from 'axios';
-import styled from 'styled-components'; 
+import styled from 'styled-components';
+import AdminContext from '../contexts/AdminContext';
 
 export const AboutMeContainer = styled.div``;
 
@@ -14,6 +15,9 @@ export default function AboutMe({ firstname, lastname, role, about}) {
     const [myAbout, setMyAbout] = useState(about);
     const [editAbout, setEditAbout] = useState(false);
     const [saveAbout, setSaveAbout] = useState(false);
+
+    const { isAdmin } = useContext(AdminContext);
+     
 
     useEffect(() => {
         const saveRoleChanges = async () => {
@@ -55,18 +59,28 @@ export default function AboutMe({ firstname, lastname, role, about}) {
             (
               <>
                 <input id='myRole' value={myRole} onChange={(event) => (setMyRole(event.target.value))}/>
-                <Icon name='check' onClick={() => {
+                {
+                isAdmin && (
+                  <>
+                 <Icon name='check' onClick={() => {
                     setSaveRole(true);
                     setEditRole(false)
                 }} />
                 <Icon name='x' onClick={() => setEditRole(false)} />
+                </>
+                )
+              }
               </>
             )
             : 
             (
               <>
                 <h4>{myRole}</h4>
+                {
+                isAdmin && (
                 <Icon name='edit' onClick={() => setEditRole(true)}/>
+                )
+              }
               </>
             )
           }
@@ -77,18 +91,28 @@ export default function AboutMe({ firstname, lastname, role, about}) {
           (
             <>
               <textarea id='myAbout' value={myAbout} onChange={(event) => (setMyAbout(event.target.value))}/>
-              <Icon name='check' onClick={() => {
-                  setSaveAbout(true);
-                  setEditAbout(false)
-              }} />
-              <Icon name='x' onClick={() => setEditAbout(false)} />
+              {
+                isAdmin && (                  
+                <>
+                  <Icon name='check' onClick={() => {
+                      setSaveAbout(true);
+                      setEditAbout(false)
+                  }} />
+                  <Icon name='x' onClick={() => setEditAbout(false)} />
+                  </>
+                )
+              }
             </>
           )
           : 
           (
             <>
               <p>{myAbout}</p>
-              <Icon name='edit' onClick={() => setEditAbout(true)}/>
+              {
+                isAdmin && (
+                  <Icon name='edit' onClick={() => setEditAbout(true)}/>
+                )
+              }
             </>
           )
         }
